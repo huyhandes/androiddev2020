7cc185a8c92d447bf74d2bbbc7c528c3d409e69f
huyhandes <huybq.b8072@st.usth.edu.vn>
2019-11-15 15:08:25 +0700

    Labwork 4 finally done

--- app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
@@ -1,22 +1,43 @@
 package vn.edu.usth.weatheractivity;
-
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = new View(getContext());
-        v.setBackgroundColor(0xFFFF0000);
+        View v = inflater.inflate(R.layout.activity_weather, null,false);
+        LinearLayout linearLayout = v.findViewById(R.id.main_activity);
+        TextView day = new TextView(getActivity());
+        day.setText("Thursday");
+        day.setPadding(10,10,10,10);
+        day.setTextSize(35);
+        day.setTypeface(null, Typeface.BOLD);
+        day.setGravity(Gravity.CENTER);
+        ImageView weatherIcon = new ImageView(getActivity());
+        weatherIcon.setImageResource(R.drawable.dayrainyful);
+        weatherIcon.setScaleType(ImageView.ScaleType.CENTER);
+        weatherIcon.setScaleX((float) 0.5);
+        weatherIcon.setScaleY((float) 0.5);
+        weatherIcon.setAdjustViewBounds(true);
+
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setGravity(Gravity.CENTER);
+        linearLayout.addView(day);
+        linearLayout.addView(weatherIcon);
+
+        v.setBackgroundColor(0xFFFFFFFF);
         return v;
     }
 }
--- app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
@@ -6,65 +6,45 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class WeatherActivity extends AppCompatActivity {
 
     private final static String TAG = "TestActivity";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ForecastFragment test = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.main_activity,test).commit();
         Log.i(TAG, "On Create .....");
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
-    /* (non-Javadoc)
-     * @see android.app.Activity#onDestroy()
-     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "On Destroy .....");
     }
-    /* (non-Javadoc)
-     * @see android.app.Activity#onPause()
-     */
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "On Pause .....");
     }
-
-    /* (non-Javadoc)
-     * @see android.app.Activity#onRestart()
-     */
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.i(TAG, "On Restart .....");
     }
-
-    /* (non-Javadoc)
-     * @see android.app.Activity#onResume()
-     */
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "On Resume .....");
     }
-
-    /* (non-Javadoc)
-     * @see android.app.Activity#onStart()
-     */
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "On Start .....");
     }
-    /* (non-Javadoc)
-     * @see android.app.Activity#onStop()
-     */
     @Override
     protected void onStop() {
         super.onStop();
