f4a9cbd19bb5a060a6a417aba071f33d7370c835
huyhandes <huybq.b8072@st.usth.edu.vn>
2019-11-23 09:46:49 +0700

    Labwork 5

--- app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
@@ -17,27 +17,17 @@ import android.widget.TextView;
 public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_weather, null,false);
+
+        final String days[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"};
+
+        ViewGroup v = (ViewGroup) inflater.inflate(R.layout.activity_weather,null,false);
         LinearLayout linearLayout = v.findViewById(R.id.main_activity);
-        TextView day = new TextView(getActivity());
-        day.setText("Thursday");
-        day.setPadding(10,10,10,10);
-        day.setTextSize(35);
-        day.setTypeface(null, Typeface.BOLD);
-        day.setGravity(Gravity.CENTER);
-        ImageView weatherIcon = new ImageView(getActivity());
-        weatherIcon.setImageResource(R.drawable.dayrainyful);
-        weatherIcon.setScaleType(ImageView.ScaleType.CENTER);
-        weatherIcon.setScaleX((float) 0.5);
-        weatherIcon.setScaleY((float) 0.5);
-        weatherIcon.setAdjustViewBounds(true);
-
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setGravity(Gravity.CENTER);
-        linearLayout.addView(day);
-        linearLayout.addView(weatherIcon);
-
-        v.setBackgroundColor(0xFFFFFFFF);
+        for(int i=0; i<10; ++i){
+            View row = inflater.inflate(R.layout.weather_row,container,false);
+            ((TextView)row.findViewById(R.id.day)).setText(days[i%7]);
+            ((ImageView)row.findViewById(R.id.weather)).setImageResource(R.drawable.daycloudyful);
+            linearLayout.addView(row);
+        }
         return v;
     }
 }
