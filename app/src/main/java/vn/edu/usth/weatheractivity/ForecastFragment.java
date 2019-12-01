1a65e089e6e8b5c54d5cb3c3c5818f9ef3af4cb0
huyhandes <huybq.b8072@st.usth.edu.vn>
2019-12-01 15:38:32 +0700

    Labwork 9

--- app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
@@ -20,19 +20,44 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        final String days[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"};
-
         ViewGroup v = (ViewGroup) inflater.inflate(R.layout.fragment_forecast,null,false);
         Random rd = new Random();
         LinearLayout linearLayout = v.findViewById(R.id.fragmentForcast);
         for(int i=0; i<10; ++i){
             View row = inflater.inflate(R.layout.weather_row,container,false);
-            ((TextView)row.findViewById(R.id.day)).setText(days[i%7]);
+            ((TextView)row.findViewById(R.id.day)).setText(day(i%7));
             ((TextView)row.findViewById(R.id.city)).setText("Paris");
             ((TextView)row.findViewById(R.id.temp)).setText(String.valueOf(rd.nextInt()%10+10) + "°C - " + String.valueOf(rd.nextInt()%10 +20) +"°C");
             ((ImageView)row.findViewById(R.id.weather)).setImageResource(R.drawable.daycloudyful);
             linearLayout.addView(row);
         }
         return v;
+    }
+    public String day(int i) {
+        int value = 0;
+        switch (i) {
+            case 0:
+                value = R.string.Mon;
+                break;
+            case 1:
+                value = R.string.Tue;
+                break;
+            case 2:
+                value = R.string.Wed;
+                break;
+            case 3:
+                value = R.string.Thu;
+                break;
+            case 4:
+                value = R.string.Fri;
+                break;
+            case 5:
+                value = R.string.Sat;
+                break;
+            case 6:
+                value = R.string.Sun;
+                break;
+        }
+        return getResources().getString(value);
     }
 }
