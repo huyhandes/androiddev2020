c546e3fb978cd4bc71ac399161f4e347c433cb6c
huyhandes <huybq.b8072@st.usth.edu.vn>
2019-12-04 11:21:17 +0700

    Finally can fix lab11 cuz mediaplayer block threads, thank for Dr Son

--- app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weatheractivity/WeatherActivity.java
@@ -7,15 +7,25 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
-public class WeatherActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+public class WeatherActivity extends AppCompatActivity {
+    MediaPlayer mp;
     private final static String TAG = "TestActivity";
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,43 @@ public class WeatherActivity extends AppCompatActivity {
         pager.setAdapter(adapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
+        //writeExternal();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.dauchiungoiyen);
+                mp.start();
+            }
+        }).start();
+
         Log.i(TAG, "On Create .....");
+    }
+    private void writeExternal() {
+        String filename = "dauchiungoiyen.mp3";
+        String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/";
+
+        try {
+            InputStream is = getApplicationContext().getResources().openRawResource(R.raw.dauchiungoiyen);
+            File myFile = new File(filepath + filename);
+            if(!myFile.exists()){
+                try{
+                    myFile.createNewFile();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            OutputStream fout = new FileOutputStream(myFile);
+            fout.write(is.read());
+            fout.close();
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
     @Override
     protected void onDestroy() {
